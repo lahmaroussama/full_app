2d0e19c521f3d9094c604d972c4fcd190d3c9e05
ikbel436 <ikbel.benmansour@esprit.tn>
2023-11-01 21:12:56 +0100

    test

--- crud-tuto-front/src/app/shared/Service/Product.service.ts
+++ crud-tuto-front/src/app/shared/Service/Product.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
 })
 export class ProductService {
   readonly API_URL = 'http://192.168.1.103:8088/SpringMVC/produit';
-
+ 
   constructor(private httpClient: HttpClient) { }
   getAllProducts() {
     return this.httpClient.get(`${this.API_URL}/retrieve-all-produits`)
