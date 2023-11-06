f1b41b35341d37ac38c9585b0ac59485f23a4c26
FakherD <fdoghri8@gmail.com>
2023-11-06 16:42:38 +0100

    test

--- achat/src/test/java/tn/esprit/rh/achat/OperateurServiceImplTest.java
+++ achat/src/test/java/tn/esprit/rh/achat/OperateurServiceImplTest.java
@@ -18,19 +18,10 @@ public class OperateurServiceImplTest {
 
     @Autowired
     private OperateurServiceImpl operateurService;
-/*
-    @Test
-    public void testRetrieveOperateur() {
-        // Assuming an Operateur with ID 1 exists in the database
-        Long id = 2L;
 
-        Operateur operateur = operateurService.retrieveOperateur(id);
+    
 
-        assertNotNull(operateur);
-        assertEquals("Fakher", operateur.getNom()); // Adjust the expected values
-    }
 
-*/
     @Test
     public void testAddOperateur() {
         Operateur operateur = new Operateur();
@@ -44,7 +35,7 @@ public class OperateurServiceImplTest {
         assertEquals("John", addedOperateur.getNom());
         assertEquals("Doe", addedOperateur.getPrenom());
         assertEquals("password", addedOperateur.getPassword());
-    }/*
+    }
     @Test
     public void testUpdateOperateur() {
         Operateur operateur = new Operateur();
@@ -56,6 +47,16 @@ public class OperateurServiceImplTest {
         assertEquals("UpdatedName", updatedOperateur.getNom());
     }
     @Test
+    public void testRetrieveOperateur() {
+        // Assuming an Operateur with ID 1 exists in the database
+        Long id = 1L;
+
+        Operateur operateur = operateurService.retrieveOperateur(id);
+
+        assertNotNull(operateur);
+        assertEquals("Fakher", operateur.getNom()); // Adjust the expected values
+    }
+    @Test
     public void testDeleteOperateur() {
         // Assuming an Operateur with ID 1 exists in the database
         Long id = 1L;
@@ -63,7 +64,7 @@ public class OperateurServiceImplTest {
         operateurService.deleteOperateur(id);
 
         // Verify that the Operateur is deleted (you can use Mockito to verify the delete method was called)
-    }*/
+    }
     }
 
     // Write similar test methods for other service operations
